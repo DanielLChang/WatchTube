4d1cc42487dc0f17302e079914c180c6afa1099a
DanielLChang <daniellawrencechang@gmail.com>
2017-01-09 17:17:48 -0800

    Finish project setup

---
+++ frontend/watchtube.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Root from './components/root';
+// import configureStore from './store/store';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // let store = configureStore();
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<h1>Welcome to WatchTube</h1>, root);
+});
