4578125bfdb4dd23e27d614dc2f9b307e6cc9a92
DanielLChang <daniellawrencechang@gmail.com>
2017-01-11 23:20:25 -0800

    Wrote util actions and video reducer

---
+++ frontend/actions/util_actions.js
@@ -0,0 +1,12 @@
+export const RECEIVE_ERRORS = "RECEIVE_ERRORS";
+export const CLEAR_ERRORS = "CLEAR_ERRORS";
+
+export const receiveErrors = (errors, formType) => ({
+  type: RECEIVE_ERRORS,
+  errors,
+  formType
+});
+
+export const clearErrors = () => ({
+  type: CLEAR_ERRORS
+});
---
+++ frontend/reducers/video_reducer.js
@@ -0,0 +1,31 @@
+import { RECEIVE_VIDEOS, RECEIVE_VIDEO } from '../actions/video_actions';
+import { RECEIVE_ERRORS, CLEAR_ERRORS } from '../actions/util_actions';
+
+import merge from 'lodash/merge';
+
+const _nullState = {
+  currentVideo: null
+};
+
+const VideoReducer = (state = _nullState, action) => {
+  Object.freeze(state);
+
+  let newState = merge({}, state);
+
+  switch (action.type) {
+    case RECEIVE_VIDEOS:
+      newState.list_videos = action.videos.list_videos;
+      return newState;
+    case RECEIVE_VIDEO:
+      newState.currentVideo = action.video;
+      return newState;
+    case RECEIVE_ERRORS:
+      const errors = action.errors;
+      newState.forms = merge({}, _nullState, { errors });
+      return newState;
+    default:
+      return state;
+  }
+
+
+};
