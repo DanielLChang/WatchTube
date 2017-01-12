44b01b023cefa07aed53b4c50002b7d1f3fb4471
DanielLChang <daniellawrencechang@gmail.com>
2017-01-11 17:15:00 -0800

    Write methods for video controller

--- app/controllers/api/videos_controller.rb
+++ app/controllers/api/videos_controller.rb
@@ -1,15 +1,45 @@
 class Api::VideosController < ApplicationController
 
   def index
+    @videos = Video.all
   end
 
   def create
+    if video_params[:thumbnail] === 'null'
+      @errors = ["Thumbnail can't be blank"]
+      render json: errors, status: 422
+    end
+
+    @video = Video.new(video_params)
+
+    if @video.save
+      render :create
+    else
+      render json: @video.errors.full_messages, status: 422
+    end
   end
 
   def show
+    @video = Video.find(params[:id])
+
+    if @video
+      #increment views by 1 and save
+      @video.views += 1
+      @video.save
+      render :show
+    else
+      render nil, status: 404
+    end
   end
 
   def update
+    @video = Video.find(params[:id])
+
+    if @video.update(video_params)
+      render :update
+    else
+      render json: @video.errors.full_messages, status: 422
+    end
   end
 
   def destroy
@@ -18,7 +48,13 @@ class Api::VideosController < ApplicationController
   private
 
   def video_params
-    params.require(:video).permit(:title, :description)
+    params.require(:video).permit(
+      :title,
+      :description,
+      :thumbnail,
+      :video,
+      :user_id,
+    )
   end
 
 end
