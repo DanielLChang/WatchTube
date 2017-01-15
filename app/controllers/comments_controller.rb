e71c3cc1b1a329e835ce4ade786df027b44f2f57
DanielLChang <daniellawrencechang@gmail.com>
2017-01-14 21:22:57 -0800

    Add comment route and controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,35 @@
 class CommentsController < ApplicationController
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render :create
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  def update
+    @comment = Comment.find(params[:id])
+
+    if @comment.update(comment_params)
+      render :update
+    else
+      render json: @comment.errors.full_messages, status: 422
+    end
+  end
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    render :destroy
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :video_id, :user_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: [:create, :update, :show]
     resource :session, only: [:create, :destroy]
-    resources :videos, only: [:create, :update, :show, :index, :destroy]
+    resources :videos, only: [:create, :update, :show, :index, :destroy] do
+      resources :comments, only: [:create, :update, :destroy]
+    end
   end
 
 end
